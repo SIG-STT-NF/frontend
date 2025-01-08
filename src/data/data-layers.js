940c79f5a7ab129c863007d3038cec3c5e13a30f
Nelan <nelanjorelat@gmail.com>
2025-01-08 08:48:06 +0700

    update custom maps

--- src/data/data-layers.js
+++ src/data/data-layers.js
@@ -10,19 +10,23 @@ export const dataLayers = [
     url: "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
   },
   {
-    name: "Stadia",
-    attribution: "Stadia",
-    url: "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png",
+    name: "Cycle OSM",
+    attribution: "Cycle OSM",
+    url: "https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png",
   },
   {
-    name: "Stadia Alidade Smooth Dark",
-    attribution: "Stadia",
-    url: "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+    name: "Esri World Street Map",
+    attribution: "Esri World Street Map",
+    url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
   },
   {
     name: "Esri.WorldImagery",
-    attribution:
-      "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+    attribution: "Esri World Imagery",
     url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+  },
+  {
+    name: "Esri National Geo Map",
+    attribution: "Esri National Geo Map",
+    url: "https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}",
   },
 ];
